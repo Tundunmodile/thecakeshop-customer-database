770484d09afb76703a139a3f8603063524e34624
Tundun Modile <tundunmodile@yahoo.com>
2021-11-15 23:30:56 -0800

    created clustered indexes

--- thecakeshop-createDBobjects.sql
+++ thecakeshop-createDBobjects.sql
@@ -1,5 +1,5 @@
 /* thecakeshop-createDBobjects.sql
- * Author: Tundun Modile
+ * Author: Olatundun Modile
  * Date Created: 11/08/2021
  * Description: DDL thecakeshop
  *
@@ -10,9 +10,17 @@
  *    OrderItems (ItemID, Size, Price)
  *
  * Indexes:
+ *    IX_IX_City
+ *    IX_State
  *
- * Stored Procedures
- *
+ * Stored Procedures:
+ *    ReadOrders 
+ *    ReadPendingOrders
+ *    ReadFulfilledOrders
+ *    CreateOrder 
+ *    DeleteOrder 
+ *    UpdateOrder 
+ 
 */
 
 
@@ -20,13 +28,15 @@
 /******************************************************
     Tables
 ******************************************************/
+USE cakeshop
 
 
-IF OBJECT_ID('Customers', 'U') IS NOT NULL
-    DROP TABLE Customers;
 
 IF OBJECT_ID('Orders', 'U') IS NOT NULL
     DROP TABLE Orders;
+
+IF OBJECT_ID('Customers', 'U') IS NOT NULL
+    DROP TABLE Customers;
 
 IF OBJECT_ID('Staff', 'U') IS NOT NULL
     DROP TABLE Staff;
@@ -40,8 +50,9 @@ CREATE TABLE Customers
     [CustomerID] INT NOT NULL IDENTITY PRIMARY KEY, 
     [FirstName] NVARCHAR(255) NOT NULL,
     [LastName] NVARCHAR(255) NOT NULL,
-    [Phone] INT (10) NOT NULL,
+    [Phone] CHAR (10) NOT NULL,
     [Email] NVARCHAR(255) NOT NULL,
+	[Address 1] NVARCHAR(255) NOT NULL,
     [City] NVARCHAR(255) NOT NULL,
     [State] NVARCHAR(2) NOT NULL,
     [Zip] NVARCHAR(5) NOT NULL
@@ -49,22 +60,6 @@ CREATE TABLE Customers
 GO
 
 
-CREATE TABLE Orders
-(
-    [OrderID] INT NOT NULL IDENTITY PRIMARY KEY, 
-    [CustomerID] INT NOT NULL,
-    [ItemID] INT NOT NULL,
-    [Quantity] INT NOT NULL,
-    [OrderStatus] NVARCHAR(255),
-    [OrderDate] DATETIME(16),
-    [RequiredDate] DATETIME(16),
-    [ShipDate] DATETIME(16),
-    [StaffID] INT NOT NULL,
-    FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID),
-    FOREIGN KEY(StaffID) REFERENCES Staff(StaffID),
-    FOREIGN KEY(ItemID) REFERENCES OrderItem(ItemID)
-);
-GO
 
 
 CREATE TABLE Staff
@@ -73,8 +68,8 @@ CREATE TABLE Staff
     [FirstName] NVARCHAR(255),
     [LastName] NVARCHAR(255),
     [Email] NVARCHAR(255),
-    [Phone] INT(10),
-    [Status] NVARCHAR(255)
+    [Phone] CHAR(10),
+    [Status] BIT
 );
 GO
 
@@ -82,9 +77,85 @@ GO
 CREATE TABLE OrderItems
 (
     [ItemID] INT NOT NULL IDENTITY PRIMARY KEY,
-    [Size] NVARCHAR(255) NOT NULL,
+    [Size] NVARCHAR(50) NOT NULL,
     [Price] MONEY
 );
 GO
 
+CREATE TABLE Orders
+(
+    [OrderID] INT NOT NULL IDENTITY PRIMARY KEY, 
+    [CustomerID] INT NOT NULL,
+    [ItemID] INT NOT NULL,
+    [Quantity] INT NOT NULL,
+    [OrderStatus] NVARCHAR(255),
+    [OrderDate] SMALLDATETIME,
+    [RequiredDate] SMALLDATETIME,
+    [ShipDate] SMALLDATETIME,
+    [StaffID] INT NOT NULL,
+    FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY(StaffID) REFERENCES Staff(StaffID),
+    FOREIGN KEY(ItemID) REFERENCES OrderItems(ItemID)
+);
+GO
 
+/******************************************************
+    Indexes
+******************************************************/
+
+
+CREATE NONCLUSTERED INDEX IX_City ON Customers (City DESC)
+GO
+
+
+CREATE NONCLUSTERED INDEX IX_State ON Customers ([State] DESC)
+GO
+
+
+
+/******************************************************
+    Stored Procedures
+******************************************************/
+
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'ReadOrders' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE ReadOrders
+GO
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'ReadPendingOrders' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE ReadPendingOrders
+GO
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'ReadFulfilledOrders' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE ReadFulfilledOrders
+GO
+
+IF EXISTS ( SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'CreateOrder' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE CreateOrder
+GO
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'DeleteOrder' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE DeleteOrder
+GO
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = N'UpdateOrder' AND ROUTINE_TYPE = N'PROCEDURE')
+DROP PROCEDURE UpdateOrder
+GO
+
+
+
+/** 
+    Stored Procedure: ReadOrders 
+    Usage: Returns a result set of orders in the database optionally filtered by CustomerID, StaffID or RequiredDate
+    Parameters:
+        @CustomerID (optional) - filters results to only include all order from a specific customer
+        @StaffID (optional) - filters rsults to only include all orders created by a specific Staff
+		@RequiredDate(optional) - filters results to only include orders due on a certain date
+
+**/
+CREATE PROCEDURE ReadOrders @CustomerID INT = NULL, @staffID INT = NULL, @RequiredDate SMALLDATETIME = NULL AS
+BEGIN
+    SET NOCOUNT ON;
+    IF @CustomerID IS NULL
+    BEGIN
+        
--- thecakeshop-loadsampledata.sql
+++ thecakeshop-loadsampledata.sql
@@ -4,34 +4,38 @@ Date Created: 11/08/2021
 Description: sample data for the cake shop
 */
 
+
+USE cakeshop
+
 INSERT INTO Customers
-VALUES ('Amina', ‘Salau’, 4562348956, ‘amina@tcs.com’, ‘123 salad street’, ‘bentonville’, ‘NE’, 45234),
-('Busayo’, ‘Aderibigbe', 4562348956, ‘busayo@tcs.com’, ‘123 salad street’, ‘bentonville’, ‘NE’, 45234),
-('Bose’, ‘Sunday', 4562348956, ‘bose@tcs.com’, ‘123 salad street’, ‘bentonville’, ‘NE’, 45234),
-('Nnamdi’, ‘Waziri', 4562348956, ‘nnamdi@tcs.com’, ‘123 salad street’, ‘bentonville’, ‘NE’, 45234),
-('Asokoro’, ‘Ijanikin', 4562348956, ‘asokoro@tcs.com’, ‘123 salad street’, ‘bentonville’, ‘NE’, 45234),
-('Tolu’, ‘Ogunbiyi', 4562348956, ‘tolu@tcs.com’, ‘123 salad street’, ‘bentonville’, ‘NE’, 45234),
-('Dinah’, ‘Steele', 4562348956, ‘dinah@tcs.com’, ‘123 salad street’, ‘bentonville’, ‘NE’, 45234);
+VALUES ('Amina','Salau', 4562348956, 'amina@tcs.com', '123 salad street', 'bentonville', 'NE', 45234),
+('Busayo', 'Aderibigbe', 4562348956, 'busayo@tcs.com', '123 salad street', 'bentonville', 'NE', 45234),
+('Bose', 'Sunday', 4562348956, 'bose@tcs.com', '123 salad street', 'bentonville', 'NE', 45234),
+('Nnamdi', 'Waziri', 4562348956, 'nnamdi@tcs.com', '123 salad street', 'bentonville', 'NE', 45234),
+('Asokoro', 'Ijanikin', 4562348956, 'asokoro@tcs.com', '123 salad street', 'bentonville', 'NE',  45234),
+('Tolu', 'Ogunbiyi', 4562348956, 'tolu@tcs.com', '123 salad street', 'bentonville', 'NE', 45234),
+('Dinah', 'Steele', 4562348956, 'dinah@tcs.com', '123 salad street', 'bentonville', 'NE', 45234);
 GO
 
 INSERT INTO Orders
-VALUES (1, 5, ‘pending’, 08/22/2021 00:00:000, 12/06/2021 00:00:000, 1),
-(2, 5, 'fulfilled', 08/24/2021 00:00:000, 09/30/2021 00:00:000, 09/30/2021 00:00:000, 1),
-(3, 3, 'pending', 10/05/2021 00:00:000, 11/27/2021 00:00:000, 2),
-(4, 1, ‘fulfilled’, 10/05/2021 00:00:000, 10/10/2021 00:00:000, 10/10/2021 00:00:000, 1),
-(4, 3, ‘fulfilled’, 10/05/2021 00:00:000, 10/31/2021 00:00:000, 10/30/2021 00:00:000, 2),
-(5, 1, ‘pending’, 11/01/2021 00:00:000, 11/27/2021 00:00:000, 2);
+VALUES 
+(1, 5, NULL, 'pending', 08/22/2021 00:00:00, 12/06/2021 00:00:00, NULL, 1),
+(2, 5, NULL, 'fulfilled', 08/24/2021 00:00:00, 09/30/2021 00:00:00, 09/30/2021 00:00:00, 1),
+(3, 3, NULL, 'pending', 10/05/2021 00:00:00, 11/27/2021 00:00:00, NULL, 2),
+(4, 1, NULL, 'fulfilled', 10/05/2021 00:00:00, 10/10/2021 00:00:00, 10/10/2021 00:00:00, 1),
+(4, 3, NULL, 'fulfilled', 10/05/2021 00:00:00, 10/31/2021 00:00:00, 10/30/2021 00:00:00, 2),
+(5, 1, NULL, 'pending', 11/01/2021 00:00:000, 11/27/2021 00:00:00, NULL, 2);
 GO
 
 INSERT INTO Staff
-VALUES ('Tundun', ‘Modile’, ‘tundun@tcs.com’, 2348646543, ‘active’),
-('Bianca', ‘Smith’, ‘bianca@tcs.com’, 8542563478, ‘active’);
+VALUES ('Tundun', 'Modile', 'tundun@tcs.com', 2348646543, '1'),
+('Bianca', 'Smith', 'bianca@tcs.com', 8542563478, '1');
 GO
 
-INSERT INTO Order_Items
-VALUES (1, ‘one tier’, $200),
-(2, ‘two tiers’, $280),
-(3, ‘three tiers’, $350),
-(4, ‘four tiers’, $420),
-(5, ‘five tiers’, $500);
+INSERT INTO OrderItems
+VALUES ('8"', $200),
+('10"', $280),
+('12"', $350),
+('14"', $420),
+('16"', $500);
 GO
